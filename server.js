1bfebc3c898394d25f9fce4e06456de73d7415f9
moon <msj07199@gmail.com>
2023-11-02 15:50:53 +0900

    test

--- server.js
+++ server.js
@@ -29,25 +29,25 @@ app.use("/", boxofficeRouter);
 const performanceRouter = require("./api/performanceData");
 app.use("/", performanceRouter);
 
-// 멤버 정보 라우팅
-const memberRouter = require("./routes/member");
-app.use("/", memberRouter);
+// // 멤버 정보 라우팅
+// const memberRouter = require("./routes/member");
+// app.use("/", memberRouter);
 
-// 리뷰 라우팅
-const reviewRouter = require("./routes/reviews");
-app.use("/", reviewRouter);
+// // 리뷰 라우팅
+// const reviewRouter = require("./routes/reviews");
+// app.use("/", reviewRouter);
 
-// 예약 라우팅
-const reservationRouter = require("./routes/reservation");
-app.use("/", reservationRouter);
+// // 예약 라우팅
+// const reservationRouter = require("./routes/reservation");
+// app.use("/", reservationRouter);
 
-// 회원가입 라우팅
-const signupRouter = require("./routes/signup");
-app.use("/", signupRouter);
+// // 회원가입 라우팅
+// const signupRouter = require("./routes/signup");
+// app.use("/", signupRouter);
 
-// 로그인 라우팅
-const loginRouter = require("./routes/login");
-app.use("/", loginRouter);
+// // 로그인 라우팅
+// const loginRouter = require("./routes/login");
+// app.use("/", loginRouter);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server is running on port ${process.env.SERVER_PORT}`);
